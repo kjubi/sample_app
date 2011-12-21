458d837a0633a5edee345f7b1e4ea3b2ef3bb988
kjubi <estarshono@gmail.com>
2011-12-21 15:05:42 +1100

    Added page name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
-  def contact
-  end
+	def contact
+  	@title = "Contacts"
+	end
+  
+	def about
+  	@title = "About"
+ 	end
 
 end
